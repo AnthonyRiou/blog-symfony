40d59748b69ef39cf21ddd50c66a0e2bd7672da8
AnthonyRiou <138130661+AnthonyRiou@users.noreply.github.com>
2024-04-02 13:58:09 +0200

    Premier pas sur le CRUD

---
+++ src/Controller/Admin/BlogController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Posts;
+use App\Form\PostType;
+use App\Repository\PostsRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+// Définition d'une route : 
+#[Route('', name: 'admin_blog')]
+final class BlogController extends AbstractController
+{
+    /**
+     * Méthode du controlleur pour affiche la page index.
+     * 
+     * Cette méthode est chargée de rendre la page d'index du blog dans le panneau d'administration.
+     * @Route('', name='index', methods= ['GET'])
+     * 
+     * @param PostsRepository : $repository Le dépôt permettant d'asséder aux données des articles.
+     * 
+     * @return Response : Retourne un objet Response contenant le HTML rendu pour la page index.
+     */
+    #[Route('', name: 'index', methods: ['GET'])]
+    public function index(PostsRepository $repository): Response 
+    {
+        $posts = $repository->findAll();
+        return $this->render('admin/blog/index.html.twig', [
+        'posts' => $posts
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $em): Response 
+    {
+        $post = new Posts();
+        $form = $this-> createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // $em = entity_manager;
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute('admin_blog_index', status: Response::HTTP_SEE_OTHER);
+            // L'ajout de Response::HTTP_SEE_OTHER est une convention aujourd'hui en matère de réponse.
+        }
+        return $this->render('admin/blog/new.html.twig', [
+            'form' => $form,
+            'post' =>$post
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Posts $post, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('admin_blog_index',
+            [
+                'id'=> $post->getId()
+            ],
+             status: Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('admin/blog/edit.html.twig', [
+            'form' => $form,
+            'post' => $post
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Posts $post): Response
+    {
+        return $this->render('admin/blog/show.html.twig', [
+            'post' => $post
+        ]);
+    }
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Posts;
+use App\Entity\Tags;
+use App\Entity\Users;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('summary')
+            ->add('content');
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Posts::class,
+        ]);
+    }
+}
